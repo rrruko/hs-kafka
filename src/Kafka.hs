ca4122bfd5257ec140a3611b25b11b34211055d2
Ethan Jones <joneetha@oregonstate.edu>
2019-07-05 10:36:27 -0400

    generate an ostensibly valid request

--- src/Kafka.hs
+++ src/Kafka.hs
@@ -32,18 +32,6 @@ data KafkaException = KafkaException String -- change later
 newKafka :: Endpoint -> IO (Either (ConnectException Uninterruptible) Kafka)
 newKafka = fmap (fmap Kafka) . connect
 
-  {-
-Produce Request (Version: 7) => transactional_id acks timeout [topic_data]
-  transactional_id => NULLABLE_STRING
-  acks => INT16
-  timeout => INT32
-  topic_data => topic [data]
-    topic => STRING
-    data => partition record_set
-      partition => INT32
-      record_set => RECORDS
-  -}
-
 testMessage :: IO ByteArray
 testMessage =
   let
@@ -54,19 +42,48 @@ testMessage =
       [ byteArrayFromList [toBE16 $ 0] -- produce api key 0
       , byteArrayFromList [toBE16 $ 7] -- api_version
       , byteArrayFromList [toBE32 $ 0] -- correlation_id
-      , byteArrayFromList [toBE16 $ -1] -- client_id nullable_string length
+      , byteArrayFromList [toBE16 $ 4] -- client_id nullable_string length
+      , byteArrayFromList (unpack $ encodeUtf8 $ "ruko")
       ]
     msgdata =
       [ byteArrayFromList [toBE16 $ -1] -- transactional_id length
-      , byteArrayFromList [toBE16 $ 0] -- acks
-      , byteArrayFromList [toBE32 $ 1000] -- timeout in ms
+      , byteArrayFromList [toBE16 $ 1] -- acks
+      , byteArrayFromList [toBE32 $ 30000] -- timeout in ms
       , byteArrayFromList [toBE32 $ 1] -- following array length
       , byteArrayFromList [toBE16 $ 4] -- following string length
       , byteArrayFromList (unpack $ encodeUtf8 $ "test") -- topic_data topic
-      , byteArrayFromList [toBE32 $ 1] -- following array length
+      , byteArrayFromList [toBE32 $ 1] -- following array [data] length
       , byteArrayFromList [toBE32 $ 0] -- partition
-      , byteArrayFromList [toBE32 $ 123] -- record set length
+      , byteArrayFromList [toBE32 $ 0x48] -- record_set length
+      , recordBatch (mkRecord (unpack $ encodeUtf8 $ "awoo"))
       ]
+    mkRecord content = fold
+      [ byteArrayFromList [0x14 :: Word8] -- length
+      , byteArrayFromList [0 :: Word8] -- attributes
+      , byteArrayFromList [0 :: Word8] -- timestampdelta varint
+      , byteArrayFromList [0 :: Word8] -- offsetDelta varint
+      , byteArrayFromList [1 :: Word8] -- key length varint
+      -- no key because key length is -1
+      , byteArrayFromList [8 :: Word8] -- value length varint
+      , byteArrayFromList content -- value
+      , byteArrayFromList [0 :: Word8] --headers
+      ]
+    recordBatch records = fold
+      [ byteArrayFromList [toBE64 $ 0] -- baseOffset
+      , byteArrayFromList [toBE32 $ 0x3c] -- batchLength
+      , byteArrayFromList [toBE32 $ 0] -- partitionLeaderEpoch
+      , byteArrayFromList [2 :: Word8] -- magic
+      , byteArrayFromList [toBE32 $ 0] -- crc
+      , byteArrayFromList [toBE16 $ 0] -- attributes
+      , byteArrayFromList [toBE32 $ 0] -- lastOffsetDelta
+      , byteArrayFromList [toBE64 $ 0] -- firstTimestamp
+      , byteArrayFromList [toBE64 $ -1] -- lastTimestamp
+      , byteArrayFromList [toBE64 $ -1] -- producerId
+      , byteArrayFromList [toBE16 $ -1] -- producerEpoch
+      , byteArrayFromList [toBE32 $ -1] -- baseSequence
+      , byteArrayFromList [toBE32 $ 1] -- records array length
+      ]
+      <> records
   in 
     pure $ fold $ size <> msghdr <> msgdata
   where
@@ -82,7 +99,9 @@ doStuff = do
       let topic = Topic (byteArrayFromList "topic") 0 a
       arr <- newUnliftedArray 1 msg
       farr <- freezeUnliftedArray arr 0 1
-      produce k topic 10000000 farr
+      v <- produce k topic 10000000 farr
+      print v
+      pure $ second (const ()) v
     Left bad -> print bad *> pure (Right ())
 
 produce ::
@@ -90,7 +109,7 @@ produce ::
   -> Topic
   -> Int -- number of microseconds to wait for response
   -> UnliftedArray ByteArray -- payloads
-  -> IO (Either KafkaException ())
+  -> IO (Either KafkaException Int)
 produce kafka topic waitTime payloads = do
   interrupt <- registerDelay waitTime
   arr <- newByteArray 100
@@ -108,23 +127,11 @@ produce kafka topic waitTime payloads = do
       100
       mba
       0
-  pure (second (const ()) response)
+  print =<< unsafeFreezeByteArray mba
+  pure response
 
 toKafkaException :: ReceiveException 'Interruptible -> KafkaException
 toKafkaException = KafkaException . show
 
 foldByteArrays :: UnliftedArray ByteArray -> ByteArray
 foldByteArrays = foldrUnliftedArray (<>) (byteArrayFromList ([]::[Char]))
-
-{-
-  send
-    interrupt
-    (getKafka kafka)
-    (foldrUnliftedArray (\ba acc -> acc) _ payloads)
-  bytesReceived <- 
-    receiveOnce
-      interrupt 
-      (getKafka kafka)
-      1024
-  print bytesReceived
--}
