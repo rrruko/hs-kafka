71796295f3a9a2228d5d19b8cd9da090ad68c617
Ethan Jones <joneetha@oregonstate.edu>
2019-07-12 14:14:55 -0400

    delete unused function

--- src/Kafka.hs
+++ src/Kafka.hs
@@ -1,4 +1,7 @@
-module Kafka where
+module Kafka
+  ( produce
+  , fetch
+  ) where
 
 import Data.Bifunctor (first)
 import Data.ByteString (ByteString)
@@ -22,13 +25,6 @@ produce ::
 produce kafka topic waitTime payloads = do
   let message = produceRequest (waitTime `div` 1000) topic payloads
   first toKafkaException <$> sendMany (getKafka kafka) message
-
-produce' :: UnliftedArray ByteArray -> ByteString -> IO ()
-produce' bytes topicName = do
-  topic <- Topic (fromByteString topicName) 0 <$> newIORef 0
-  Right k <- newKafka (Peer (IPv4 0) 9092)
-  _ <- produce k topic 30000000 bytes
-  pure ()
 
 fetch ::
      Kafka
