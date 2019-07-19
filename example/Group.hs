e36b2f29f8545753f5b6cafb2f0ab8d68a6a3163
Ethan Jones <joneetha@oregonstate.edu>
2019-07-19 16:02:36 -0400

    example of assigning state from leader to follower

--- example/Group.hs
+++ example/Group.hs
@@ -4,11 +4,13 @@
 
 module Main where
 
+import Control.Concurrent
 import Control.Monad
 import Data.Either (isLeft)
 import Data.IORef
 import Data.Primitive.ByteArray (ByteArray)
 import GHC.Conc
+import System.IO.Unsafe (unsafePerformIO)
 
 import Kafka
 import Kafka.Common
@@ -16,26 +18,84 @@ import Kafka.Heartbeat.Response
 import Kafka.JoinGroup.Response
 import Kafka.SyncGroup.Response
 
+groupName :: ByteArray
+groupName = fromByteString "example-consumer-group"
+
+children :: MVar [MVar ()]
+children = unsafePerformIO (newMVar [])
+
+waitForChildren :: IO ()
+waitForChildren = do
+  cs <- takeMVar children
+  case cs of
+    [] -> pure ()
+    m:ms -> do
+      putMVar children ms
+      takeMVar m
+      waitForChildren
+
 main :: IO ()
 main = do
-  let name = fromByteString "example-consumer-group-1"
-  (t, kafka) <- setup name
+  forkFollower (follower "1")
+  forkFollower (follower "2")
+  leader
+  waitForChildren
+
+forkFollower :: IO () -> IO ()
+forkFollower io = do
+  mvar <- newEmptyMVar
+  childs <- takeMVar children
+  putMVar children (mvar:childs)
+  void $ forkFinally io (\_ -> putMVar mvar ())
+
+leader :: IO ()
+leader = do
+  (t, kafka) <- setup groupName
+  let
+    Topic topicName partitionCount _ = t
+    partitions groupMembers i =
+      filter
+        (\n -> mod (n + i) (length groupMembers) == 0)
+        [0..partitionCount]
+    assign groupMembers =
+      fmap
+        (\(member, i) ->
+          MemberAssignment (fromByteString $ groupMemberId member)
+            [TopicAssignment (topicName) (fromIntegral <$> partitions groupMembers i)])
+        (zip groupMembers [0..])
   case kafka of
     Nothing -> putStrLn "Failed to connect to kafka"
     Just k -> do
-      let member = GroupMember name Nothing
-      (genId, newMember) <- initGroupConsumer k t member
+      let member = GroupMember groupName Nothing
+      (genId, newMember, allMembers) <- initGroupConsumer k t member
       interrupt <- registerDelay 5000000
-      case newMember of
-        GroupMember _ (Just memberName) -> do
-          void $ syncGroup k newMember genId
-            [ MemberAssignment memberName
-                [TopicAssignment (fromByteString "test") [0]]
-            ]
-          registerDelay 3000000 >>= getSyncGroupResponse k >>= print
-          heartbeats k t newMember genId interrupt
-        _ -> do
-          putStrLn "The server didn't assign a member id"
+      putStrLn "Leader is making the following assignments:"
+      print (assign allMembers)
+      void $ syncGroup k newMember genId $ assign allMembers
+      registerDelay 3000000 >>= getSyncGroupResponse k >>= \case
+        Right (Right sgr) -> do
+          putStrLn ("leader: my assigned topics are " <> show (memberAssignment sgr))
+          heartbeats k t newMember genId interrupt "leader"
+        e -> do
+          print e
+
+follower :: String -> IO ()
+follower name = do
+  (t, kafka) <- setup groupName
+  case kafka of
+    Nothing -> putStrLn "Failed to connect to kafka"
+    Just k -> do
+      let member = GroupMember groupName Nothing
+      (genId, newMember, _) <- initGroupConsumer k t member
+      interrupt <- registerDelay 5000000
+      void $ syncGroup k newMember genId
+        []
+      registerDelay 3000000 >>= getSyncGroupResponse k >>= \case
+        Right (Right sgr) -> do
+          putStrLn (name <> ": my assigned topics are " <> show (memberAssignment sgr))
+          heartbeats k t newMember genId interrupt name
+        e -> do
+          print e
 
 setup :: ByteArray -> IO (Topic, Maybe Kafka)
 setup topicName = do
@@ -50,26 +110,27 @@ heartbeats ::
   -> GroupMember
   -> GenerationId
   -> TVar Bool
+  -> String
   -> IO ()
-heartbeats kafka top member genId interrupt = do
+heartbeats kafka top member genId interrupt name = do
   wait <- registerDelay 2000000
   void $ heartbeat kafka member genId
   resp <- getHeartbeatResponse kafka wait
   threadDelay 1000000
   case resp of
-    Right (Right r) -> do
-      print r
+    Right (Right _) -> do
+      putStrLn ("heartbeat (" <> name <> ")")
       halt <- atomically $ readTVar interrupt
       when (not halt) $ do
-        heartbeats kafka top member genId interrupt
+        heartbeats kafka top member genId interrupt name
     e -> do
-      print e
+      putStrLn "Failed to receive heartbeat response"
 
 initGroupConsumer ::
      Kafka
   -> Topic
   -> GroupMember
-  -> IO (GenerationId, GroupMember)
+  -> IO (GenerationId, GroupMember, [Member])
 initGroupConsumer kafka top member@(GroupMember groupName _) = do
   wait <- registerDelay 5000000
   ex <- joinGroup kafka top member
@@ -84,7 +145,7 @@ initGroupConsumer kafka top member@(GroupMember groupName _) = do
         Right (Right jgr2) -> do
           print jgr2
           let genId = GenerationId (generationId jgr2)
-          pure (genId, assignment)
+          pure (genId, assignment, members jgr2)
         Right (Left e) -> do
           fail ("Failed parsing join group response: " <> show e)
         Left e -> do
