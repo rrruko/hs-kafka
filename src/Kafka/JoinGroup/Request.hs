a42bbe4f8d21a4d14cfda23a8f04218a1b3f7276
Ethan Jones <joneetha@oregonstate.edu>
2019-07-19 11:18:16 -0400

    fix incorrect buffer size

--- src/Kafka/JoinGroup/Request.hs
+++ src/Kafka/JoinGroup/Request.hs
@@ -34,12 +34,14 @@ defaultProtocolData :: ByteArray -> forall s. KafkaWriterBuilder s
 defaultProtocolData topic = mconcat
   [ build32 1 -- 1 protocol
   , buildString (fromByteString "range") 5 -- protocol name
-  , build32 16 -- metadata bytes length
+  , build32 (12 + fromIntegral topicSize) -- metadata bytes length
   , build16 0 -- version
   , build32 1 -- number of subscriptions
-  , buildString topic (sizeofByteArray topic) -- topic name
+  , buildString topic topicSize -- topic name
   , build32 0 -- userdata bytes length
   ]
+  where
+    topicSize = sizeofByteArray topic
 
 joinGroupRequest ::
      Topic
