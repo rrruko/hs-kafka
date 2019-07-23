bbc3d8a533db4789bef5133b9cd98458d1ce28df
Ethan Jones <joneetha@oregonstate.edu>
2019-07-23 09:43:05 -0400

    serialize OffsetCommit requests

---
+++ src/Kafka/OffsetCommit/Request.hs
@@ -0,0 +1,53 @@
+module Kafka.OffsetCommit.Request
+  ( offsetCommitRequest
+  ) where
+
+import Data.List
+import Data.Primitive.ByteArray
+import Data.Primitive.Unlifted.Array
+
+import Kafka.Common
+import Kafka.Writer
+
+serializePartition :: PartitionOffset -> KafkaWriterBuilder s
+serializePartition a = foldBuilder
+  [ build32 (partitionIndex a)
+  , build64 (partitionOffset a)
+  , build32 (-1) -- leader epoch
+  , build32 (-1) -- metadata
+  ]
+
+offsetCommitRequest ::
+     TopicName
+  -> [PartitionOffset]
+  -> GroupMember
+  -> GenerationId
+  -> UnliftedArray ByteArray
+offsetCommitRequest topic offs groupMember generationId =
+  let
+    GroupMember gid mid = groupMember
+    TopicName topicName = topic
+    GenerationId genId = generationId
+    reqSize = evaluate $ foldBuilder [build32 (size32 req)]
+    req =
+      evaluate $ foldBuilder
+        [ buildString gid (sizeofByteArray gid)
+        , build32 genId
+        , maybe
+            (build16 0)
+            (\m -> buildString m (sizeofByteArray m))
+            mid
+        , build32 1 -- 1 topic
+        , buildString topicName (sizeofByteArray topicName)
+        , build32 (fromIntegral $ length offs)
+        , foldl'
+            (\acc e -> acc <> serializePartition e)
+            mempty
+            offs
+        ]
+  in
+    runUnliftedArray $ do
+      arr <- newUnliftedArray 2 mempty
+      writeUnliftedArray arr 0 reqSize
+      writeUnliftedArray arr 1 req
+      pure arr
