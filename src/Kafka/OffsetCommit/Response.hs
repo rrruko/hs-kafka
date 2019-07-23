80d8617838f3722feaacb7993c0bf36521e2d22d
Ethan Jones <joneetha@oregonstate.edu>
2019-07-23 10:01:35 -0400

    parse OffsetCommit response

---
+++ src/Kafka/OffsetCommit/Response.hs
@@ -0,0 +1,56 @@
+module Kafka.OffsetCommit.Response
+  ( OffsetCommitPartition(..)
+  , OffsetCommitResponse(..)
+  , OffsetCommitTopic(..)
+  , getOffsetCommitResponse
+  , parseOffsetCommitPartitions
+  ) where
+
+import Data.Attoparsec.ByteString (Parser, (<?>))
+import Data.ByteString
+import Data.Int
+import GHC.Conc
+
+import Kafka.Combinator
+import Kafka.Common
+import Kafka.Response
+
+data OffsetCommitResponse = OffsetCommitResponse
+  { throttleTimeMs :: Int32
+  , offsetCommitTopics :: [OffsetCommitTopic]
+  } deriving (Eq, Show)
+
+data OffsetCommitTopic = OffsetCommitTopic
+  { offsetCommitTopicName :: ByteString
+  , offsetCommitPartition :: [OffsetCommitPartition]
+  } deriving (Eq, Show)
+
+data OffsetCommitPartition = OffsetCommitPartition
+  { offsetCommitPartitionIndex :: Int32
+  , offsetCommitErrorCode :: Int16
+  } deriving (Eq, Show)
+
+parseOffsetCommitResponse :: Parser OffsetCommitResponse
+parseOffsetCommitResponse = do
+  _correlationId <- int32 <?> "correlation id"
+  OffsetCommitResponse
+    <$> (int32 <?> "throttle time")
+    <*> (array parseOffsetCommitTopic <?> "topics")
+
+parseOffsetCommitTopic :: Parser OffsetCommitTopic
+parseOffsetCommitTopic = do
+  OffsetCommitTopic
+    <$> (byteString <?> "topic name")
+    <*> (array parseOffsetCommitPartitions <?> "partitions")
+
+parseOffsetCommitPartitions :: Parser OffsetCommitPartition
+parseOffsetCommitPartitions = do
+  OffsetCommitPartition
+    <$> (int32 <?> "partition id")
+    <*> (int16 <?> "error code")
+
+getOffsetCommitResponse ::
+     Kafka
+  -> TVar Bool
+  -> IO (Either KafkaException (Either String OffsetCommitResponse))
+getOffsetCommitResponse = fromKafkaResponse parseOffsetCommitResponse
