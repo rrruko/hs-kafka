393029f5085d5aa2ec15a3111b70575af611a091
Ethan Jones <joneetha@oregonstate.edu>
2019-07-23 10:40:58 -0400

    serialize OffsetFetch requests

---
+++ src/Kafka/OffsetFetch/Request.hs
@@ -0,0 +1,44 @@
+module Kafka.OffsetFetch.Request where
+
+import Data.Int
+import Data.List
+import Data.Primitive.ByteArray
+import Data.Primitive.Unlifted.Array
+
+import Kafka.Common
+import Kafka.Writer
+
+offsetFetchApiKey :: Int16
+offsetFetchApiKey = 9
+
+offsetFetchApiVersion :: Int16
+offsetFetchApiVersion = 5
+
+offsetFetchRequest ::
+     GroupMember
+  -> TopicName
+  -> [Int32]
+  -> UnliftedArray ByteArray
+offsetFetchRequest (GroupMember gid _) (TopicName topicName) offs =
+  let
+    reqSize = evaluate $ foldBuilder [build16 (size16 req)]
+    req =
+      evaluate $ foldBuilder
+        [ build16 offsetFetchApiKey
+        , build16 offsetFetchApiVersion
+        , buildString (fromByteString clientId) clientIdLength
+        , buildString gid (sizeofByteArray gid)
+        , build32 1 -- 1 topic
+        , buildString topicName (sizeofByteArray topicName)
+        , build32 (fromIntegral (length offs))
+        , foldl'
+            (\acc e -> acc <> build32 e)
+            mempty
+            offs
+        ]
+  in
+    runUnliftedArray $ do
+      arr <- newUnliftedArray 2 mempty
+      writeUnliftedArray arr 0 reqSize
+      writeUnliftedArray arr 1 req
+      pure arr
