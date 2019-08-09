2dbfee36ac1dce861c4a4c9b88a67dd36e9b6099
Ethan Jones <joneetha@oregonstate.edu>
2019-08-09 09:55:06 -0400

    benchmark fetch request construction

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -8,47 +8,72 @@ import Data.Primitive.ByteArray
 import Gauge
 
 import Kafka.Common
+import Kafka.Fetch.Request
 import Kafka.Produce.Request
 
-data RequestData =
-  RequestData
-    { timeout :: Int
-    , topic :: TopicName
-    , partition :: Int32
-    , payloads :: UnliftedArray ByteArray
+data ProduceArgs =
+  ProduceArgs
+    { pTimeout :: Int
+    , pTopic :: TopicName
+    , pPartition :: Int32
+    , pPayloads :: UnliftedArray ByteArray
     }
 
-produceRequest' :: RequestData -> UnliftedArray ByteArray
-produceRequest' (RequestData {..}) =
-  produceRequest timeout topic partition payloads
+produceRequest' :: ProduceArgs -> UnliftedArray ByteArray
+produceRequest' (ProduceArgs {..}) =
+  produceRequest pTimeout pTopic pPartition pPayloads
+
+shortPayloads = unliftedArrayFromList
+  [ fromByteString "aaaaa"
+  , fromByteString "bbbbb"
+  , fromByteString "ccccc"
+  , fromByteString "ddddd"
+  , fromByteString "eeeee"
+  ]
+
+shortProduceArgs =
+  ProduceArgs
+    30000000
+    (TopicName (fromByteString "test"))
+    0
+    shortPayloads
+
+longPayloads = unliftedArrayFromList
+  [ runST $ newByteArray (10*1000*1000) >>= unsafeFreezeByteArray
+  ]
+
+longProduceArgs =
+  ProduceArgs
+    30000000
+    (TopicName (fromByteString "test"))
+    0
+    longPayloads
+
+data FetchArgs =
+  FetchArgs
+    { fTimeout :: Int
+    , fTopic :: TopicName
+    , fPartitions :: [PartitionOffset]
+    }
+
+fetchProduceArgs =
+  FetchArgs
+    30000000
+    (TopicName (fromByteString "test"))
+    [PartitionOffset 0 0, PartitionOffset 1 0, PartitionOffset 2 0]
+
+fetchRequest' :: FetchArgs -> UnliftedArray ByteArray
+fetchRequest' (FetchArgs {..}) =
+  sessionlessFetchRequest fTimeout fTopic fPartitions
 
 main :: IO ()
 main = do
-  let shortPayloads = unliftedArrayFromList
-        [ fromByteString "aaaaa"
-        , fromByteString "bbbbb"
-        , fromByteString "ccccc"
-        , fromByteString "ddddd"
-        , fromByteString "eeeee"
-        ]
-      shortRequestData =
-        RequestData
-          30000000
-          (TopicName (fromByteString "test"))
-          0
-          shortPayloads
-      longPayloads = unliftedArrayFromList
-        [ runST $ newByteArray (10*1000*1000) >>= unsafeFreezeByteArray
-        ]
-      longRequestData =
-        RequestData
-          30000000
-          (TopicName (fromByteString "test"))
-          0
-          longPayloads
   defaultMain
     [ bgroup "produceRequest"
-        [ bench "short payloads" $ whnf produceRequest' shortRequestData
-        , bench "long payloads" $ whnf produceRequest' longRequestData
+        [ bench "short payloads" $ whnf produceRequest' shortProduceArgs
+        , bench "long payloads" $ whnf produceRequest' longProduceArgs
+        ]
+    , bgroup "fetchRequest"
+        [ bench "fetch" $ whnf fetchRequest' fetchProduceArgs
         ]
     ]
